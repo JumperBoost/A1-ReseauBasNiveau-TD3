2a1a0ed56434815633d54a763561633626708606
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-02-09 10:35:31 +0100

    Implémentation de la Q1/Q2 (readfile)

--- readfile.c
+++ readfile.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
 
 /**
  * Ouvre le fichier message.txt du répertoire courant et affiche son contenu à l'écran.
  */
 int main() {
-
+    chdir("..");    // Obligatoire si utilisation de Cmake (pour définir le bon working directory)
+    int fd = open("message.txt", O_RDONLY);
+    if(fd < 0) {
+        printf("Une erreur est survenue lors de la lecture du fichier.\n");
+    } else {
+        char buffer[670];
+        int size = read(fd, buffer, 669);
+        buffer[size] = '\0';
+        //printf("%s", buffer);
+        write(1, buffer, size);
+    }
 }
