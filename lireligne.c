5ec31a0f3d235763e6b16c824e5475f9385085bc
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-02-29 15:50:02 +0100

    Fin de 'lireligne'

--- lireligne.c
+++ lireligne.c
@@ -12,19 +12,29 @@
  * @return le nombre d'octets effectivement lus
  */
 int lireligne(int fd, char *s, int size) {
-    int i = 0;
     if(fd < 0)
         return -1;
-    char c;
-    while (i < size && read(fd, &c, 1) > 0 && c != '\n')
-        s[i++] = c;
+    int old_size = lseek(fd, 0, SEEK_CUR);
+    int read_size = read(fd, s, size);
+    int i = 0;
+    while (i < read_size && s[i] != '\n')
+        i++;
+    s[i] = '\0';
+    printf("%i, %i\n", read_size, i);
+    lseek(fd, old_size + i+2, SEEK_SET);
     return i;
 }
 
 int main() {
     chdir("..");    // Obligatoire si utilisation de Cmake (pour définir le bon working directory)
     int fd = open("message.txt", O_RDONLY);
-    char buffer[670];
-    printf("%i\n", lireligne(fd, buffer, 100));
+    char buffer[4096];
+    printf("%i\n", lireligne(fd, buffer, 4096));
+    printf("%s\n", buffer);
+    printf("%i\n", lireligne(fd, buffer, 4096));
+    printf("%s\n", buffer);
+    printf("%i\n", lireligne(fd, buffer, 4096));
+    printf("%s\n", buffer);
+    printf("%i\n", lireligne(fd, buffer, 4096));
     printf("%s\n", buffer);
 }
